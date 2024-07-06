d30a6a67cf53b2c0cbf4e774dde02ba8a66145f3
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-07-06 15:08:49 -0400

    create transaction component complete

--- src/components/NewTransaction.jsx
+++ src/components/NewTransaction.jsx
@@ -1,5 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function NewTransaction() {
-    return <h1>New Transaction</h1>
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    amount: 0,
+    date: "",
+    from: "",
+    category: "",
+  });
+
+  function createTransaction(transaction) {
+    const options = {
+      method: "POST",
+      body: JSON.stringify(transaction),
+      headers: { "Content-Type": "application/json" },
+    };
+    return fetch("http://localhost:6298/transactions", options).then(
+      (response) => response.json()
+    );
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTransaction(form).then((newTransaction) => {
+      navigate(`/transactions/${newTransaction.id}`); // redirect to the created transaction's show page
+    });
+  };
+
+  return (
+    <div>
+      <h1>New Transaction</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="amount">Amount:</label>
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={form.amount}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="date">Date:</label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={form.date}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="from">From:</label>
+          <input
+            type="text"
+            id="from"
+            name="from"
+            value={form.from}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="category">Category:</label>
+          <input
+            type="text"
+            id="category"
+            name="category"
+            value={form.category}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Create Transaction</button>
+      </form>
+    </div>
+  );
 }
